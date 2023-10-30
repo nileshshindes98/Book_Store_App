6570e9a4ab6f6700302631df37b8ee95050c8bae
nileshshindes98 <nileshshindes98@gmail.com>
2023-10-30 12:38:18 +0530

    [Nilesh] : Updated Login Component with css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, ValidationErrors, FormControl, FormBuilder, FormGroup, Validators, } from '@angular/forms';
+import {
+  AbstractControl,
+  ValidationErrors,
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { UserService } from 'src/app/Services/User Services/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTabChangeEvent } from '@angular/material/tabs';
@@ -8,47 +15,35 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   mainContainerColor = '#707070'; // Default background color
 
   hide = true;
   login!: FormGroup;
   signup!: FormGroup;
 
-  changeMainContainerBgCOlor: boolean = false;
-
-
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
     private snackBar: MatSnackBar,
     private route: Router
-  ) {
-
-  }
+  ) {}
 
   ngOnInit(): void {
     this.login = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    })
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
 
     this.signup = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      mobNo: ['', [Validators.required, Validators.pattern('[789][0-9]{9}')]]
-    })
+      mobNo: ['', [Validators.required, Validators.pattern('[789][0-9]{9}')]],
+    });
   }
-
-
-
-  onTabChange() {
-    this.changeMainContainerBgCOlor = !this.changeMainContainerBgCOlor
-  }
-
 
   loginUser() {
     if (this.login.valid) {
@@ -56,29 +51,27 @@ export class LoginComponent implements OnInit{
 
       let sendData = {
         email: this.login.value.email,
-        password: this.login.value.password
-      }
+        password: this.login.value.password,
+      };
 
       console.log(sendData);
-
 
       this.userService.login(sendData).subscribe((res: any) => {
         console.log(res);
         this.snackBar.open(res.message, '', {
-          duration: 2000
+          duration: 2000,
         });
 
         localStorage.setItem('token', res.result.accessToken);
 
-        this.route.navigateByUrl('/dashboard')
-
-      })
+        this.route.navigateByUrl('/dashboard');
+      });
     } else {
       this.snackBar.open("please enter valid credential's", '', {
-        duration: 2000
+        duration: 2000,
+        verticalPosition:'top'
       });
     }
-
   }
 
   registerNewUser() {
@@ -87,19 +80,17 @@ export class LoginComponent implements OnInit{
         fullName: this.signup.value.name,
         email: this.signup.value.email,
         password: this.signup.value.password,
-        phone: this.signup.value.mobNo
-      }
+        phone: this.signup.value.mobNo,
+      };
 
       console.log(sendData);
 
       this.userService.registerNewUser(sendData).subscribe((res: any) => {
         console.log(res);
         this.snackBar.open(res.message, '', {
-          duration: 2000
+          duration: 2000,
         });
-      })
-
+      });
     }
   }
-
 }
