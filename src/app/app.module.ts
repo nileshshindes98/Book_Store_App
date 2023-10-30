c00e03386febcb841fbe590f5a5c58123f297f6a
nileshshindes98 <nileshshindes98@gmail.com>
2023-10-30 12:39:28 +0530

    [Nilesh] : add separate module (dashboard.module) into a AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LoginModule } from './components/login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardModule } from './components/dashboard/dashboard.module';
+
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule],
+  declarations: [AppComponent, ],
+  imports: [BrowserModule, AppRoutingModule, LoginModule,DashboardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
