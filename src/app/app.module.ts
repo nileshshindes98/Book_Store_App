56aceb0fbd1a5db003af016eccfc09ece2a318ef
nileshshindes98 <nileshshindes98@gmail.com>
2023-11-02 12:56:57 +0530

    [Nilesh] : import module of newly added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { LoginModule } from './components/login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './components/dashboard/dashboard.module';
-
+import { RouterModule } from '@angular/router';
+import { DisplayAllBooksModule } from './components/display-all-books/display-all-books.module';
+import { GetAllBooksModule } from './components/get-all-books/get-all-books.module';
 
 @NgModule({
   declarations: [AppComponent, ],
-  imports: [BrowserModule, AppRoutingModule, LoginModule,DashboardModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule,LoginModule,DashboardModule,GetAllBooksModule,DisplayAllBooksModule],
   providers: [],
   bootstrap: [AppComponent]
 })
