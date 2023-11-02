84f8228c3ced9a27625ad3624684438d0fd21359
nileshshindes98 <nileshshindes98@gmail.com>
2023-11-02 12:51:16 +0530

    [Nilesh] : Updated dashboard and set the router-outlet for get-all-books components

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { DashboardComponent } from './dashboard.component';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [MatToolbarModule,MatMenuModule],
+    imports: [MatToolbarModule,MatMenuModule,RouterModule],
     exports: [DashboardComponent],
 })
 export class DashboardModule { }
