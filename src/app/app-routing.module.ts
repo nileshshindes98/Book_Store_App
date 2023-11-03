9082d8a5f995920f3fdb2ca4c9876f69fa1675b1
nileshshindes98 <nileshshindes98@gmail.com>
2023-11-03 16:40:59 +0530

    [Nilesh] : added routing for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GetAllBooksComponent } from './components/get-all-books/get-all-books.component';
-import { AuthguardService } from './Services/AuthGuard Service/authguard.service';
+import { QuickViewBookDataComponent } from './components/quick-view-book-data/quick-view-book-data.component';
+import { AuthGuard } from './AuthGuard/auth-guard.guard';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [AuthguardService], // Protect the dashboard route
+    path: 'dashboard',component: DashboardComponent,canActivate: [AuthGuard], // Protect the dashboard route
     children: [
       { path: '', component: GetAllBooksComponent },
-      // Add more child routes as needed, and protect them with AuthGuardService
+      { path: 'quickView', component: QuickViewBookDataComponent,canActivate:[AuthGuard] },
+      { path: 'cart', component: CartComponent,canActivate:[AuthGuard] },
+      
     ],
   },
 ];
