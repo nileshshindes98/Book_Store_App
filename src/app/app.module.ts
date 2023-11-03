e3e70778261845c35eb4062c8c6addbd4ede710f
nileshshindes98 <nileshshindes98@gmail.com>
2023-11-03 16:42:16 +0530

    [Nilesh] : import new module of newly created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { DashboardModule } from './components/dashboard/dashboard.module';
 import { RouterModule } from '@angular/router';
 import { DisplayAllBooksModule } from './components/display-all-books/display-all-books.module';
 import { GetAllBooksModule } from './components/get-all-books/get-all-books.module';
+import { QuickViewBookDataComponent } from './components/quick-view-book-data/quick-view-book-data.component';
+import { QuickViewBookDataModule } from './components/quick-view-book-data/quick-view-book-data.module';
+import { CardModule } from './components/cart/cart.module';
+
 
 @NgModule({
-  declarations: [AppComponent, ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule,LoginModule,DashboardModule,GetAllBooksModule,DisplayAllBooksModule],
+  declarations: [AppComponent ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule,LoginModule,DashboardModule,GetAllBooksModule,DisplayAllBooksModule,QuickViewBookDataModule,CardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
